7c9d730378341db6fdc4504414acb4ac76b2e01e
Anderson Fernandes <cabula21@yahoo.com.br>
2023-11-08 22:54:38 -0300

    UNIKIDS - Botao Grupo

--- js/script.js
+++ js/script.js
@@ -30,3 +30,42 @@ $(document).ready(function(){
   });
 
 
+
+  /*BOTAO GRUPO*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
+  
+/*POP UP*/
+
+  $(document).ready(function() {
+    $("#abrirPopup").click(function() {
+        var popup = window.open("", "Popup", "width=300, height=200");
+        $(popup.document.body).html(`
+            <ul class="collection">
+                <li class="collection-item">
+                    <h2><i class="large material-icons"></i> Grupo:</h2>
+                </li>
+                <li class="collection-item">
+                    <i class="large material-icons"></i>
+                    <span>Anderson Fernandes</span>
+                </li>
+                <li class="collection-item">
+                    <i class="large material-icons"></i>
+                    <span>Gabriel Lima</span>
+                </li>
+            </ul>
+        `);
+    });
+});
+
+
